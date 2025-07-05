0e3d529a9b73646fdf0784216fdf75387b5e19c3
snowari <70631889+snowari@users.noreply.github.com>
2025-07-06 01:05:08 +0900

    💄 style : 브라운관 효과 백그라운드 이미지 적용

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,11 @@
 import Header from "../components/Header/Header";
+import BackgroundWrapper from "../components/BackgroundWrapper";
 
 export default function Home() {
   return (
-    <>
+    <BackgroundWrapper>
       <Header />
       <h1>home Component</h1>
-    </>
+    </BackgroundWrapper>
   );
 }
